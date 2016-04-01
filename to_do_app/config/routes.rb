b1ceb6591cc9ba8828544d76f5745876d6a3ab1c
Michael Finneran <mrfinneran@gmail.com>
2016-04-01 18:58:06 -0400

    fixed route bug

--- to_do_app/config/routes.rb
+++ to_do_app/config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
 
-  get 'about' => 'pages#about'
+  get 'about' => 'pages#about', as: :pages_about
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
